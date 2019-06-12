11cc98679b52f9a8a061a24ed2015f70be2018ba
syhan11 <sueyoung.6311@gmail.com>
2019-06-12 09:16:51 -0400

    First commit

---
+++ src/MathTwo.java
@@ -0,0 +1,9 @@
+public class MathTwo {
+    public static void main (String[] arg) {
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+
+        System.out.println("mynumber is " + mynumber);
+        System.out.println("myothernumber is " + myothernumber);
+    }
+}
